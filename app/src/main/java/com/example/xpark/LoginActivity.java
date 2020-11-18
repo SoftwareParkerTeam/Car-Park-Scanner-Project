9295627808fba80a5ccf9c4a9cb6fe69b0fc606d
root2a <gkt.akn@gmail.com>
2020-11-18 13:09:12 +0300

    integ

--- app/src/main/java/com/example/xpark/LoginActivity.java
+++ app/src/main/java/com/example/xpark/LoginActivity.java
@@ -1,19 +1,12 @@
 package com.example.xpark;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+
 
 public class LoginActivity extends AppCompatActivity {
 
