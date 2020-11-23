c2acf6189808d9bc5e9b27bef105efe8129c355b
root2a <gkt.akn@gmail.com>
2020-11-23 23:21:00 +0300

    bug fix

--- app/src/main/java/com/example/xpark/LoginActivity.java
+++ app/src/main/java/com/example/xpark/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.xpark;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +22,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
+            // Activity was brought to front and not created,
+            // Thus finishing this will get us to the last viewed activity
+            finish();
+            return;
+        }
 
         /* initialize UI components */
         UI_init();
@@ -75,5 +84,9 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
          */
+    }
+    protected void onResume()
+    {
+        super.onResume();
     }
 }
--- app/src/main/java/com/example/xpark/MainActivity.java
+++ app/src/main/java/com/example/xpark/MainActivity.java
@@ -11,4 +11,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
     }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+    protected void onRestoreInstanceState(Bundle savedInstanceState) { super.onRestoreInstanceState(savedInstanceState); }
+    @Override
+    protected void onPause(){super.onPause();}
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        /* ignore it */
+    }
 }
