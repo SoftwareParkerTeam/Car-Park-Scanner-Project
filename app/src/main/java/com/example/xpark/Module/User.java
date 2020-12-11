888767c9d2638fe4f188c2ed67e32c919b464eed
Elif Öztürk <62054314+eelifozturk@users.noreply.github.com>
2020-12-11 13:03:42 +0300

    user class updated

--- app/src/main/java/com/example/xpark/Module/User.java
+++ app/src/main/java/com/example/xpark/Module/User.java
@@ -7,18 +7,13 @@ package com.example.xpark.Module;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    /** User's Name */
-    private String name;
-    /** User's Surname */
-    private String surName;
-    /** User's Nick Name */
-    private String nick_name;
+
+    /** User's Email */
+    private final String eMail;
     /** User's Password */
     private String password;
     /** User's Phone  */
     private String phone;
-    /** User's Email */
-    private String eMail;
     /** User's Credit Balance */
     private double credit_balance;
     /* uid for auth */
@@ -36,9 +31,6 @@ public class User implements Serializable {
      * Builds a user object
      */
     public User(){
-        this.name = null;
-        this.surName = null;
-        this.nick_name = null;
         this.password = null;
         this.phone = null;
         this.eMail = null;
@@ -47,18 +39,12 @@ public class User implements Serializable {
 
     /**
      * Builds a user object with given information.
-     * @param name_ User's name
-     * @param surName_ User's surname
-     * @param nick_name_ User's nick name
      * @param password_ User's password
      * @param phone_ User's phone
      * @param eMail_ User's email
      * @param credit_balance_ User's credit balance
      */
-    public User(String name_, String surName_,String nick_name_,String password_, String phone_, String eMail_, double credit_balance_){
-        this.name = name_;
-        this.surName = surName_;
-        this.nick_name = nick_name_;
+    public User(String password_, String phone_, String eMail_, double credit_balance_){
         this.password = password_;
         this.phone = phone_;
         this.eMail = eMail_;
@@ -71,8 +57,6 @@ public class User implements Serializable {
      * @param password User's password.
      */
     public User(String mail, String password){
-        this.name = null;
-        this.surName = null;
         this.password = password;
         this.eMail = mail;
         this.phone = null;
@@ -80,33 +64,13 @@ public class User implements Serializable {
     }
 
     /**
-     * Setter name
-     * @param name given User's name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-    /**
-     * Setter nick_name
-     * @param nick_name given User's nick name
-     */
-    public void setNickname(String nick_name) {
-        this.nick_name = nick_name;
-    }
-    /**
      * Setter password
      * @param password given User's password
      */
     public void setPassword(String password) {
         this.password = password;
     }
-    /**
-     * Setter surname
-     * @param surName given User's surname
-     */
-    public void setSurname(String surName) {
-        this.surName = surName;
-    }
+
     /**
      * Setter phone
      * @param phone given User's phone
@@ -114,13 +78,7 @@ public class User implements Serializable {
     public void setPhone(String phone) {
         this.phone = phone;
     }
-    /**
-     * Setter email
-     * @param eMail given User's email
-     */
-    public void setEmail(String eMail) {
-        this.eMail = eMail;
-    }
+
     /**
      * Setter credit balance
      * @param credit_balance given User's credit balance
@@ -128,20 +86,7 @@ public class User implements Serializable {
     public void setCreditbalance(double credit_balance) {
         this.credit_balance = credit_balance;
     }
-    /**
-     * Getter name
-     * @return string is user's name
-     */
-    public String getName() {
-        return name;
-    }
-    /**
-     * Getter nick_name
-     * @return string is user's nick name
-     */
-    public String getNickname() {
-        return nick_name;
-    }
+
     /**
      * Getter password
      * @return string is user's password
@@ -149,13 +94,7 @@ public class User implements Serializable {
     public String getPassword() {
         return password;
     }
-    /**
-     * Getter surname
-     * @return string is user's surname
-     */
-    public String getSurname() {
-        return surName;
-    }
+
     /**
      * Getter email
      * @return string is user's email
@@ -185,10 +124,7 @@ public class User implements Serializable {
     @Override
     public String toString() {
         return "User{" +
-                "name='" + name + '\'' +
-                ", surName='" + surName + '\'' +
-                ", nick_name='" + nick_name + '\'' +
-                ", password='" + password + '\'' +
+                "password='" + password + '\'' +
                 ", phone='" + phone + '\'' +
                 ", eMail='" + eMail + '\'' +
                 ", credit_balance=" + credit_balance +
