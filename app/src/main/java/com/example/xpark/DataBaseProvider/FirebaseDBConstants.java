f664d583be83e222c7f652e352a2f45056c3c85a
Elif Öztürk <62054314+eelifozturk@users.noreply.github.com>
2020-12-11 23:37:41 +0300

    Update FirebaseDBConstants.java

--- app/src/main/java/com/example/xpark/DataBaseProvider/FirebaseDBConstants.java
+++ app/src/main/java/com/example/xpark/DataBaseProvider/FirebaseDBConstants.java
@@ -2,6 +2,11 @@ package com.example.xpark.DataBaseProvider;
 
 public final class FirebaseDBConstants {
     public static final String DB_USER_FIELD = "USERS";
+    public static final String DB_USER_CHILD_EMAIL = "email";
+    public static final String DB_USER_CHILD_PASSWORD = "password";
+    public static final String DB_USER_CHILD_PHONE = "phone";
+    public static final String DB_USER_CHILD_CREDITBALANCE = "creditbalance";
+    public static final String DB_USER_CHILD_UID = "uid";
     public static final String DB_CARPARK_FIELD = "CAR_PARKS";
     public static final String DB_CARPARK_CHILD_COORDINATES = "coordinates";
     public static final String DB_CARPARK_CHILD_LONGITUDE = "longitude";
