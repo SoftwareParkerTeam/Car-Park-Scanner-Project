42b9009716b9b1c7e442177be3d7d1fb0150df3f
ozannyesiller <ozannyesiller@gmail.com>
2020-12-11 15:00:09 +0300

    QR Activity Added

---
+++ app/src/main/java/com/example/xpark/Activities/QRActivity.java
@@ -0,0 +1,14 @@
+package com.example.xpark.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.xpark.R;
+
+public class QRActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+    }
+}
