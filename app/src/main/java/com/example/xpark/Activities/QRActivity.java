b1da53f13a11ad8be4368cef1ce2244e0d02bf2b
ozannyesiller <ozannyesiller@gmail.com>
2020-12-11 17:53:59 +0300

    Version 0
    SignUp and Login work well

--- app/src/main/java/com/example/xpark/Activities/QRActivity.java
+++ app/src/main/java/com/example/xpark/Activities/QRActivity.java
@@ -10,6 +10,5 @@ public class QRActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
-
     }
 }
