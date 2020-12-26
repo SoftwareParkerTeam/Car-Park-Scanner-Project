30dd6ad34f327d4496e8139ab440e56894be1e10
rhasepy <ozannyesiller@gmail.com>
2020-12-26 21:31:26 +0300

    main activity: entrance debug...

--- app/src/main/java/com/example/xpark/Activities/EntranceDebugActivity.java
+++ app/src/main/java/com/example/xpark/Activities/EntranceDebugActivity.java
@@ -13,6 +13,7 @@ public class EntranceDebugActivity extends AppCompatActivity {
     private Button signup;
     private Button map;
     private Button qr;
+    private Button ent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,11 @@ public class EntranceDebugActivity extends AppCompatActivity {
         signup = findViewById(R.id.sign_up_enter);
         map = findViewById(R.id.map_enter);
         qr = findViewById(R.id.qrCode_enter);
+        ent = findViewById(R.id.entrance);
+
+        ent.setOnClickListener( view -> {
+            this.startActivity(new Intent(this, EntranceActivity.class));
+        });
 
         signin.setOnClickListener( view -> {
             Intent intent = new Intent(this, EntranceActivity.class);
