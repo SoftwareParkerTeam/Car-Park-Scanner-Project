00fe7dd51a20f2f6b97b55ad04ef3e8f046e0b8d
Dilara Karakaş <60318526+ddilarakarakas@users.noreply.github.com>
2021-01-04 12:50:05 +0300

    Update LoginActivityTest

--- app/src/androidTest/java/com/example/xpark/Activities/LoginActivityTest.java
+++ app/src/androidTest/java/com/example/xpark/Activities/LoginActivityTest.java
@@ -1,9 +1,18 @@
+
+/**
+ * Author : Dilara Karakas.
+ */
+
+
 package com.example.xpark.Activities;
 
+import android.content.Intent;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.test.rule.ActivityTestRule;
-
+import com.example.xpark.DataBaseProvider.FirebaseUserManager;
 import com.example.xpark.R;
 
 import org.junit.After;
@@ -18,6 +27,7 @@ public class LoginActivityTest {
     public ActivityTestRule<LoginActivity> loginActivityActivityTestRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
 
     private LoginActivity myLoginActivity = null;
+    private FirebaseUserManager DBUserManager;
 
     @Before
     public void setUp() throws Exception {
@@ -25,11 +35,20 @@ public class LoginActivityTest {
     }
 
     @Test
-    public void testLaunch(){
+    public void testID(){
         ImageView imageView = myLoginActivity.findViewById(R.id.imageView);
-        System.out.println(myLoginActivity.findViewById(R.id.imageView));
         assertNotNull(imageView);
+        Button sign_in_button = myLoginActivity.findViewById(R.id.sign_in_button);
+        assertNotNull(sign_in_button);
+        Button sign_up_button = myLoginActivity.findViewById(R.id.sign_up_button);
+        assertNotNull(sign_up_button);
+        TextView email_input = myLoginActivity.findViewById(R.id.userName);
+        assertNotNull(email_input);
+        TextView password_input = myLoginActivity.findViewById(R.id.Password);
+        assertNotNull(password_input);
     }
+
+
 
     @After
     public void tearDown() throws Exception {
